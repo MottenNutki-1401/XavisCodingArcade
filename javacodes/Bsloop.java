4d664da9858594ff8bfc89365f44a5b3dcc5824f
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-01-27 18:02:52 +0800

    Reverse 1-10 usig 4loop

---
+++ javacodes/Bsloop.java
@@ -0,0 +1,11 @@
+package javacodes;
+
+public class Bsloop{
+    public static void main(String[] args){
+
+        for (int i =10; i>0; i--) {
+            System.out.println(i);
+        }
+    }
+    
+}
