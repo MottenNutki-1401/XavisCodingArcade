87416b513648c0261019f07978f39defa213e3a0
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-01-27 18:33:08 +0800

    Inifinite string inside 4loop structure

--- javacodes/StringFor.java
+++ javacodes/StringFor.java
@@ -9,3 +9,5 @@ public class StringFor {
     }
     
 }
+//Note: This code will run infinetly because the condition will always be true since the string "hello world" will always have a length greater than 2
+//we need an updater like (int i--;) but increment or decrement onlyworks with numbers and not strings DT
