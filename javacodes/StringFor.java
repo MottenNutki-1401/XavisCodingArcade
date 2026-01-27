bb2278e1e7ad0696c274762995fac451f6894064
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-01-27 18:21:12 +0800

    Infinite string inside for loop structure

---
+++ javacodes/StringFor.java
@@ -0,0 +1,11 @@
+package javacodes;
+
+public class StringFor {
+    public static void main (String[] args) {
+
+        for (String i= "hello world"; i.length() >2;){
+            System.out.println(i);
+        }
+    }
+    
+}
