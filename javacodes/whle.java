2df63b0668045dc2ed8b7186178da18241664e63
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-01-27 12:36:24 +0800

    added java practices

---
+++ javacodes/whle.java
@@ -0,0 +1,13 @@
+package javacodes;
+public class whle {
+    public static void main(String[] args) {
+
+        int  wh = 10;
+
+        while(wh>0){
+            System.out.println(wh); // if i turn it to string then the chars i put here will be printed instead of numbers
+            wh--;
+        }
+    }
+    
+}
