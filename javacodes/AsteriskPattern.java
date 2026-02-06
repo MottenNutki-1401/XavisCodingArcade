8af9e926b5b67d5a9f723556baed8ec634303b82
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-02-07 00:41:28 +0800

    pattern exercise (right triangle) using nested for loop

---
+++ javacodes/AsteriskPattern.java
@@ -0,0 +1,14 @@
+package javacodes;
+
+public class AsteriskPattern {
+    public static void main(String[] args){
+
+        for (int i =0; i<=6;i++){
+            for (int j=1;j<i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
+//print a right triangle shape asterisk pattern
