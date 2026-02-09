29d24b5d5e54877751ff85e9d9067a000e5e88e5
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-02-09 23:43:13 +0800

    object box dino moves up using KeyListener

---
+++ dinodump/DinoDay2.java
@@ -0,0 +1,81 @@
+package dinodump;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class DinoDay2{
+    public static void main (String [] args){
+
+        JFrame gameF = new JFrame();
+
+        gameF.setSize(700,400);
+        gameF.setTitle("DinoRawr!!!");
+        gameF.setLocationRelativeTo(null);
+
+
+
+        GamePanel containF = new GamePanel();
+
+        containF.setSize(699,399);
+        containF.setBackground(Color.decode("#000260"));
+        gameF.add(containF);
+        gameF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameF.setVisible(true);
+
+    }
+}
+//creating a new class called GamePanel
+    // extends JPanel=> means we make GamePanel a JPanel 
+    //@Override => we're replacing a method from a parent class with our 
+    //own version in this case its a (JPanel)
+ class GamePanel extends JPanel implements KeyListener{ //i got an error when i put it inside the DinoMain class so i had to put it outside
+        
+    //draw dino (logic only) position + size
+    int dinoX = 50;
+    int dinoY= 100;
+    int dinoWidth= 45;
+    int dinoHeight= 45;
+
+                //key press logic / Constructor 
+                        public GamePanel () { 
+                        setFocusable(true);
+                        addKeyListener(this);
+                        }
+    
+    @Override 
+        protected void paintComponent (Graphics g) { //you can change any letter here
+            super.paintComponent(g); //paintCompnent is a reserve keyword
+
+            //drawing dino using the variable constraints (from above)
+            g.setColor(Color.decode("#fffd80"));
+            g.fillRect(dinoX,dinoY,dinoWidth,dinoHeight);
+    
+        } 
+
+        //key Listener Methods
+    @Override 
+    public void keyPressed (KeyEvent e){
+           if (e.getKeyCode() == KeyEvent.VK_SPACE){
+            dinoY -= 80;
+            
+            repaint();
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+    
+    @Override
+     public void keyTyped(KeyEvent e){
+        
+     }
+            
+    }
+//Quick Note:
+/*Protected means:
+
+This method or variable can be used by:
+The class itself (obviously)
+Subclasses (classes that extend this class)
+Other classes in the same package */
