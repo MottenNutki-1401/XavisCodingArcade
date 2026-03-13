1443b91fbb28f8654908edba2348ca149b4348d8
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-03-13 14:25:10 +0800

    hash map like (dictionary)

---
+++ python/dictionaryArray.py
@@ -0,0 +1,10 @@
+#creating dictionary (like a hash)
+studnt = {
+    "name": "Gail Parazo",
+    "idNum": "202310763",
+    'course': 'BSCS 3B' #left side = key, right side = value
+}
+#print
+print(studnt["name"])
+print(studnt["idNum"])
+print(studnt["course"])
