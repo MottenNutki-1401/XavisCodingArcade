0c9a5eab3210ca5a372774a42431b79d2a0c305d
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-03-13 14:22:23 +0800

    showing data types

---
+++ python/dt.py
@@ -0,0 +1,11 @@
+#declaring variables
+age = 23
+height = 185.6
+name = 'Xavier'
+goodAtCooking = False
+
+#print variables + tell its type
+print (age, "Type:", type(age))
+print(height, "Type:", type(height))
+print(name, "Type:", type(name))
+print(goodAtCooking, "Type:", type(goodAtCooking))
